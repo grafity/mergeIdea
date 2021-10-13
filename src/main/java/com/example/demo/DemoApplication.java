01fbb6bfb4354dff66d9cc232f30e392c5f8e96c
jlafuente <jlafuente@post.com>
2021-10-14 00:19:43 +0200

    sysout

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static java.time.Clock.system;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        System.out.println("WWW");
     }
 
 }
